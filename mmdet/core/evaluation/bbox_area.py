7c81b22e1bed4453df53dc095152423a496c6ccf
dt_chenzhen <15382001028@163.com>
2022-11-04 09:36:32 +0800

    bound_loss

--- mmdet/core/evaluation/bbox_area.py
+++ mmdet/core/evaluation/bbox_area.py
@@ -16,9 +16,7 @@ def bbox_area(bboxes1, area_size):
     area1 = (bboxes[:, 2] - bboxes[:, 0]) * (bboxes[:, 3] - bboxes[:, 1])
 
     for i in range(bboxes1.shape[0]):
-
         if area_size[0] <= area1[i] <= area_size[1]:
-
             det_bbox.append(bboxes1[i])
 
     return np.array(det_bbox)
