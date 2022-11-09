c4c5ef972ded15b300822bdccb1a825333603972
triple-Mu <gpu@163.com>
2022-11-09 17:34:10 +0800

    Format code

--- train.py
+++ train.py
@@ -27,9 +27,7 @@ config = '/home/chenzhen/code/detection/mmdetection/configs/datang_detection/yol
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
     parser.add_argument(
-        '--config',
-        help='train config file path',
-        default=config)
+        '--config', help='train config file path', default=config)
     parser.add_argument('--work-dir', help='the dir to save logs and models')
     parser.add_argument(
         '--resume-from', help='the checkpoint file to resume from')
