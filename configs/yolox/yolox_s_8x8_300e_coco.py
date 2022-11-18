98c362afaae894e48e1746b24064cae8fd48e418
triple-Mu <gpu@163.com>
2022-11-18 11:05:21 +0800

    remove global var

--- configs/yolox/yolox_s_8x8_300e_coco.py
+++ configs/yolox/yolox_s_8x8_300e_coco.py
@@ -157,7 +157,6 @@ evaluation = dict(
     interval=interval,
     dynamic_intervals=[(max_epochs - num_last_epochs, 1)],
     metric='bbox')
-
 log_config = dict(interval=50)
 
 # NOTE: `auto_scale_lr` is for automatically scaling LR,
