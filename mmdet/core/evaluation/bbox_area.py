c85d30c074ea307f47bc040506919d3db70b0a26
paihuai0 <845960230@qq.com>
2022-12-28 17:04:47 +0800

    add hook

--- mmdet/core/evaluation/bbox_area.py
+++ mmdet/core/evaluation/bbox_area.py
@@ -1,22 +1,26 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 import numpy as np
 
-
-def bbox_area(bboxes1, area_size):
+def bbox_area(bboxes1,
+              area_size):
     """Calculate the area each bbox of bboxes1 and bboxes2.
 
-    Args:
-    bboxes1 (ndarray): Shape (k, 4)
+        Args:
+        bboxes1 (ndarray): Shape (k, 4)
     """
     bboxes = bboxes1[:, :4]
     bboxes = bboxes.astype(np.float32)
 
     det_bbox = []
 
-    area1 = (bboxes[:, 2] - bboxes[:, 0]) * (bboxes[:, 3] - bboxes[:, 1])
+    area1 = (bboxes[:, 2] - bboxes[:, 0]) * (
+                bboxes[:, 3] - bboxes[:, 1])
 
     for i in range(bboxes1.shape[0]):
+
         if area_size[0] <= area1[i] <= area_size[1]:
+
             det_bbox.append(bboxes1[i])
+
 
     return np.array(det_bbox)
